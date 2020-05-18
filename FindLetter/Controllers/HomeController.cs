d7fc3128e7b770e0030b60f8245043b2ba9af721
nkhakwan <post_khan@yahoo.com>
2020-05-18 14:05:28 -0700

    committing code in two new cshtml files

--- FindLetter/Controllers/HomeController.cs
+++ FindLetter/Controllers/HomeController.cs
@@ -22,6 +22,18 @@ namespace FriendLetter.Controllers
       myLetterVariable.Sender = "Jasmine";
       return View(myLetterVariable);
     }
+
+    [Route("/form")]
+    public ActionResult Form() { return View(); }
+
+    [Route("/postcard")]
+    public ActionResult Postcard(string recipient, string sender)
+    {
+      LetterVariable myLetterVariable = new LetterVariable();
+      myLetterVariable.Recipient = recipient;
+      myLetterVariable.Sender = sender;
+      return View(myLetterVariable);
+    }
     
 
   }
